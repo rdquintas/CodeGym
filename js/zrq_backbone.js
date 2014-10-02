11e91ce36e9bd6a90dc13cc29c1862899e7715da
Ricardo Quintas <rqcenter-web@example.com>
2014-10-02 16:53:58 +0100

    backbone model

--- js/zrq_backbone.js
+++ js/zrq_backbone.js
@@ -1,60 +1,43 @@
 (function() {
 
-    var zrqModel = Backbone.Model.extend({
+    window.App = {
+        Models: {},
+        Collections: {},
+        Views: {}
+    };
+
+    App.Models.zrq = Backbone.Model.extend({
         defaults: {
             task_id: new Date().valueOf(),
-            categoria: "",
-            first_name: "",
-            last_name: "",
-            data: "",
-            prioridade: "1"
+            prioridade: "0",
+            data: new Date(),
+            firt_name: "",
+            last_name: ""
         },
 
-        validate: function(args) {
-            if (args.prioridade > 5) {
-                return "erro caralho";
+        full_name: function() {
+            return this.get("firt_name") + " " + this.get("last_name");
+        },
+
+        validate: function(ats, options) {
+            if (ats.prioridade < 1 || ats.prioridade > 5) {
+                return "erro do caralho";
             }
-        },
-
-        fullname: function() {
-            return this.get("first_name") + " " + this.get("last_name");
         }
     });
 
-
-    var mainCOl = Backbone.Collection.extend({
-        model: zrqModel,
+    var zrqModel = new App.Models.zrq({
+        "firt_name": "Ricardo",
+        "last_name": "Quintas"
+            // "prioridade": "3"
     });
 
-    var zrq = new zrqModel();
-    var zrqCol = new mainCOl();
 
-    console.log(zrq.get("task_id"));
-
-    zrq.set("last_name", "Quintas");
-    console.log(zrq.fullname());
-    console.log(zrq.get("prioridade"));
-    zrq.set("prioridade", "5", {
+    console.log(zrqModel.get("firt_name"));
+    console.log(zrqModel.full_name());
+    console.log(zrqModel.get("prioridade"));
+    zrqModel.set("prioridade", "3", {
         validate: true
     });
-    console.log(zrq.get("prioridade"));
-
-    zrq.set("first_name", "Nome1");
-    zrq.set("last_name", "Apelido1");
-    zrqCol.add(zrq);
-
-    zrq = new zrqModel();
-    zrq.set("first_name", "Nome2");
-    zrq.set("last_name", "Apelido2");
-    zrqCol.add(zrq);
-
-    zrq = new zrqModel();
-    zrq.set("first_name", "Nome3");
-    zrq.set("last_name", "Apelido3");
-    zrqCol.add(zrq);
-
-    zrqCol.each(function(cena) {
-        console.log(cena.fullname());
-    });
-
+    console.log(zrqModel.get("prioridade"));
 })();
