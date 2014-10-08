54d326f9f12864bd7436e27715aabd22a3d7c254
Ricardo Quintas <rqcenter-web@yahoo.com>
2014-10-08 21:18:34 +0100

    collection views

--- js/zrq_backbone.js
+++ js/zrq_backbone.js
@@ -7,10 +7,7 @@
 
     // Models ----------------------------------
     App.Models.zrq = Backbone.Model.extend({
-        defaults: {
-            first_name: "",
-            last_name: ""
-        },
+
         validate: function() {
             if (!first_name || !last_name) {
                 return "erro do caralho";
@@ -20,14 +17,16 @@
     });
 
     // Collections ----------------------------------
+    App.Collections.zrq = Backbone.Collection.extend({
+        model: App.Models.zrq
+    });
 
     // Views ----------------------------------
-    App.Views.zrqMain = Backbone.View.extend({
+    App.Views.zrqForm = Backbone.View.extend({
         // template: _.template($("#zrqForm").html()),
         el: "#zrqForm",
         events: {
-            "submit": "btnClicked",
-            "click #btnOk": "btnClicked"
+            "click #btnOk": "addMore"
         },
         initialize: function() {
             this.render();
@@ -38,41 +37,72 @@
             return this;
         },
 
-        btnClicked: function(e) {
+        addMore: function(e) {
             e.preventDefault();
+            console.log($("#inpFirstName").val());
             console.log("clicado caralho: " + e);
         }
     });
 
-    App.Views.zrq = Backbone.View.extend({
+    App.Views.single = Backbone.View.extend({
+        tagName: "li",
+        // initialize: function() {
+        //     this.render();
+        // },
 
-        initialize: function() {
-            this.render();
-        },
+        model: App.Models.zrq,
 
         template: _.template($("#zrqTemplate").html()),
 
         render: function() {
-            this.el = this.template(this.model.toJSON());
+            // this.collection.each(function(item) {
+            //     // this.el = this.template(this.model.toJSON());
+            //     console.log(item.el);
+            // }, this);
+
             return this;
         }
     });
 
 
+    App.Views.multiple = Backbone.View.extend({
+        tagName: "ul",
+        collection: App.Collections.zrq,
+        render: function() {
+            return this;
+        }
+    });
+
     // ****************************************************
-    zrqModel = new App.Models.zrq({
+    var zrqModel = new App.Models.zrq({
         first_name: "Ricardo",
         last_name: "Quintas"
-
     });
 
-    zrqMainView = new App.Views.zrqMain({});
+    // window.zrqCol = new App.Collections.zrq();
+    // zrqCol.add(zrqModel);
 
-    zrqView = new App.Views.zrq({
-        model: zrqModel
+    window.zrqCol = new App.Collections.zrq([{
+        first_name: "Ricardo",
+        last_name: "Quintas"
+    }, {
+        first_name: "Oscar",
+        last_name: "Alho"
+    }, {
+        first_name: "Crespo",
+        last_name: "Ramos"
+    }]);
+
+    var zrqMultipleView = new App.Views.multiple({
+        collection: zrqCol
     });
+    var zrqFormView = new App.Views.zrqForm({});
 
-    $("#container").html(zrqView.render().el);
+    // var singleView = new App.Views.single({
+    //     mode: zrqCol
+    // });
+
+    // $("#container").html(zrqView.el);
 
     // console.log(zrqView.render().el);
 })();
