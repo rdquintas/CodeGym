6dbc5e1c94a969d401c09bac8f8cd79974b2b1cf
Ricardo Quintas <rqcenter-web@example.com>
2014-11-20 14:15:43 +0000

    adding node

---
+++ app/models/router.js
@@ -0,0 +1,62 @@
+// ROUTES FOR OUR API
+// =============================================================================
+var express = require('express'); // call express
+var router = express.Router(); // get an instance of the express Router
+
+// middleware to use for all requests
+router.use(function(req, res, next) {
+    // do logging
+    // console.log('Something is happening.');
+    next(); // make sure we go to the next routes and don't stop here
+});
+
+// test route to make sure everything is working (accessed at GET http://localhost:8080/api)
+router.get('/', function(req, res) {
+    // res.json({
+    //     message: 'hooray! welcome to our api!'
+    // });
+});
+
+
+// on routes that end in /bears
+// ----------------------------------------------------
+router.route('/bears')
+    .post(function(req, res) {
+        var id = server.create(req.body.name);
+        res.json({
+            message: 'Bear created with ID: ' + id
+        });
+    })
+
+.get(function(req, res) {
+    res.json(server.getAll());
+});
+
+// on routes that end in /bears/:bear_id
+// ----------------------------------------------------
+router.route('/bears/:bear_id')
+
+// get the bear with that id (accessed at GET http://localhost:8080/api/bears/:bear_id)
+.get(function(req, res) {
+    res.json(server.findById(req.params.bear_id));
+})
+
+// update the bear with this id (accessed at PUT http://localhost:8080/api/bears/:bear_id)
+.put(function(req, res) {
+
+    // use our bear model to find the bear we want
+    var id = server.findById(req.params.bear_id);
+
+    if (id) {
+        server.update(req.params.bear_id, req.body.name);
+        res.json({
+            message: 'Bear updated with new name: ' + req.body.name
+        });
+    } else {
+        res.json({
+            message: 'Invalid cena!'
+        });
+    }
+});
+
+module.exports.router = router;
