1fd2c2843c4c56267e75a04ff629cd15ad7042c8
Ricardo Quintas <rqcenter-web@yahoo.com>
2014-12-23 17:30:52 +0000

    semantic

--- semantic/js/main.js
+++ semantic/js/main.js
@@ -12,6 +12,29 @@ $(function() {
     });
 
 
+    // codigo para chamar as popups
+    $(".pop").popup({
+        // on: 'click'
+    });
+
+
+    // codigo para fazer o dimmer da imagem
+    $("#dimmer").dimmer({
+        on: "hover"
+    });
+
+    // este codigo mostra um dimmer da pagina toda quando clico na cena
+    $("#dimmer").on("click", function() {
+        $(".page.dimmer").dimmer("show");
+    });
+
+
+    // Codigo para o botao do ORDER
+    $("#btnOrder").on("click", function() {
+        $('#img1').transition('pulse', "150ms").transition("shake");
+    });
+
+
     //fechar o side-bar quando a tecla ESC for clicada    
     $(document).keyup(function(e) {
         if (e.keyCode == 27) {
