3b77c2f5548f519c11f724e7211ee899ed2d94cb
Ricardo Quintas <rqcenter-web@example.com>
2015-01-14 14:56:34 +0000

    backbone

--- backbone/nested-collections/public/js/app.js
+++ backbone/nested-collections/public/js/app.js
@@ -1,7 +1,42 @@
 var Note = Backbone.Model.extend({});
 var Notes = Backbone.Collection.extend({
     model: Note,
+    initialize: function(models, options) {
+        this.doc = options.doc;
+    },
+    url: function() {
+        return this.doc.url() + '/notes';
+    }
+});
+
+var Document = Backbone.Model.extend({
     initialize: function() {
-        this.doc =. opt ions.doc;
+        this.notes = new Notes([], {
+            doc: this
+        })
+    },
+
+    addNote: function(text) {
+        this.notes.create({
+            text: text
+        });
+    }
+});
+
+
+var Documents = Backbone.Collection.extend({
+    model: Document,
+    url: '/documents',
+    initialize: function() {
+        this.on('reset', this.getNotes, this);
+    },
+
+    getNotes: function() {
+        this.each(function(doc) {
+            doc.notes = new Notes([], {
+                doc: doc
+            })
+            doc.notes.fetch();
+        })
     }
 });
